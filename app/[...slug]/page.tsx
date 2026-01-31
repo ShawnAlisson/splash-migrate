062d669085c00d6aa11f47e196ae36cea25821c0
Shawn <73370054+ShawnAlisson@users.noreply.github.com>
2026-01-31 17:25:54 +0000

    suspense added

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SplashPage from "../../components/SplashPage";
 
 export default function CatchAllPage() {
-  return <SplashPage />;
+  return (
+    <Suspense fallback={null}>
+      <SplashPage />
+    </Suspense>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import SplashPage from "../components/SplashPage";
 
 export default function Page() {
-  return <SplashPage />;
+  return (
+    <Suspense fallback={null}>
+      <SplashPage />
+    </Suspense>
+  );
 }
